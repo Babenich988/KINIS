fcf75afd4be494b68b98be54eb6938b65b1b405f
Kancerio <ghh233406@gmail.com>
2025-11-02 17:15:58 +0300

    Revert "Добавлен метод отрисовки точек привязки"
    This reverts commit bf237f7e67a638d98d442cbb057f8c43e65e159e.

--- Models/BpmnBlock.cs
+++ Models/BpmnBlock.cs
@@ -38,19 +38,6 @@ namespace Kinis.Models
 
         }
 
-        //Метод отрисовки точек привязки
-        public void DrawConnectionPoints(Graphics g)
-        {
-            var points = GetConnectionPoints();
-            using (var brush = new SolidBrush(Color.Green))
-            {
-                foreach (var point in points)
-                {
-                    g.FillEllipse(brush, point.X - 3, point.Y - 3, 6, 6);
-                }
-            }
-        }
-
         // Метод отрисовки блока
         public void Draw(Graphics g, bool isSelected = false)
         {
