96d671e60f1454c3b4d8f0429b452651358d1503
Kancerio <ghh233406@gmail.com>
2025-11-02 19:54:52 +0300

    Добавлен метод нахождения расстояния до стрелки

--- Models/BpmnArrow.cs
+++ Models/BpmnArrow.cs
@@ -30,6 +30,40 @@ namespace Kinis.Models
             EndPoint = endPoint;
         }
 
+        //Метод нахождения расстояния до стрелки
+        private float DistanceToLine(PointF point, PointF lineStart, PointF lineEnd)
+        {
+            float A = point.X - lineStart.X;
+            float B = point.Y - lineStart.Y;
+            float C = lineEnd.X - lineStart.X;
+            float D = lineEnd.Y - lineStart.Y;
+
+            float dot = A * C + B * D;
+            float lenSq = C * C + D * D;
+            float param = (lenSq != 0) ? dot / lenSq : -1;
+
+            float xx, yy;
+
+            if (param < 0)
+            {
+                xx = lineStart.X;
+                yy = lineStart.Y;
+            }
+            else if (param > 1)
+            {
+                xx = lineEnd.X;
+                yy = lineEnd.Y;
+            }
+            else
+            {
+                xx = lineStart.X + param * C;
+                yy = lineStart.Y + param * D;
+            }
+
+            float dx = point.X - xx;
+            float dy = point.Y - yy;
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
         public void Draw(Graphics g, bool isSelected = false)
         {
             using (var pen = new Pen(isSelected ? Color.Blue : Color, isSelected ? Width + 1 : Width))
