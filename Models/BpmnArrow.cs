288ccec612453999811c6a4d42b021237b8af1ec
Kancerio <ghh233406@gmail.com>
2025-11-02 18:44:11 +0300

    Добавлена модель BpmnArrow с базовыми свойствами

--- Models/BpmnArrow.cs
+++ Models/BpmnArrow.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Kinis.Models
 {
-    internal class BpmnArrow
+    [Serializable]
+    public class BpmnArrow
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Text { get; set; } = "";
+
+        // Ссылки на блоки и точки привязки
+        public BpmnBlock StartBlock { get; set; }
+        public PointF StartPoint { get; set; }
+        public BpmnBlock EndBlock { get; set; }
+        public PointF EndPoint { get; set; }
+
+        // Визуальные свойства
+        public Color Color { get; set; } = Color.Black;
+        public float Width { get; set; } = 2f;
+
+        public BpmnArrow() { }
+
+        public BpmnArrow(BpmnBlock startBlock, PointF startPoint, BpmnBlock endBlock, PointF endPoint)
+        {
+            StartBlock = startBlock;
+            StartPoint = startPoint;
+            EndBlock = endBlock;
+            EndPoint = endPoint;
+        }
+
+        public void Draw(Graphics g, bool isSelected = false)
+        {
+            using (var pen = new Pen(isSelected ? Color.Blue : Color, isSelected ? Width + 1 : Width))
+            {
+                pen.EndCap = LineCap.ArrowAnchor;
+                pen.StartCap = LineCap.Round;
+
+                // Рисуем линию
+                g.DrawLine(pen, StartPoint, EndPoint);
+            }
+        }
     }
 }
