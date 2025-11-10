bcf99a1af5a2e0bdaab39b40aed89b343e4e7929
Kancerio <ghh233406@gmail.com>
2025-11-10 13:15:07 +0300

    Добавлен класс BlockCreationService.cs для создания блоков на хоткеи

---
+++ Models/BlockCreationService.cs
@@ -0,0 +1,22 @@
+﻿using Kinis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Kinis.Services
+{
+    public class BlockCreationService
+    {
+        private readonly InfiniteCanvas _canvas;
+        private readonly List<BpmnBlock> _blocks;
+
+        public BlockCreationService(InfiniteCanvas canvas, List<BpmnBlock> blocks)
+        {
+            _canvas = canvas;
+            _blocks = blocks;
+        }
+    }
+}
