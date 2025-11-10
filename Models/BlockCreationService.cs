4e43f61bb70a73cdb1904c5fb3bc4c4a380a4ca8
Kancerio <ghh233406@gmail.com>
2025-11-10 14:25:29 +0300

    добавлена конфигурация сопоставления клавиш

--- Models/BlockCreationService.cs
+++ Models/BlockCreationService.cs
@@ -1,10 +1,11 @@
-﻿using Kinis.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
+using System.Windows.Forms;
+using Kinis.Models;
 
 namespace Kinis.Services
 {
@@ -61,6 +62,23 @@ namespace Kinis.Services
             _blocks.Add(block);
             _canvas.SetBlocks(_blocks);
             _canvas.Invalidate();
+        }
+
+        public Dictionary<Keys, (string type, string text)> GetBlockKeyMappings()
+        {
+            return new Dictionary<Keys, (string, string)>
+            {
+                { Keys.D1, ("Комментарий", "Комментарий") },
+                { Keys.D2, ("Задача", "Задача") },
+                { Keys.D3, ("Развилка", "Развилка") },
+                { Keys.D4, ("Начальное событие", "Начальное событие") },
+                { Keys.D5, ("Промежуточное событие", "Промежуточное событие") },
+                { Keys.D6, ("Конечное событие", "Конечное событие") },
+                { Keys.D7, ("Объект данных", "Объект данных") },
+                { Keys.D8, ("Хранилище данных", "Хранилище данных") },
+                { Keys.D9, ("Arrow", "→") },
+                { Keys.D0, ("Task", "Новая задача") }
+            };
         }
     }
 }
