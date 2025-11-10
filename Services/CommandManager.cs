b9660a80ef9f817ce3a0120cef25e8a65e70241c
Kancerio <ghh233406@gmail.com>
2025-11-10 16:33:25 +0300

    реализован метод execute

--- Services/CommandManager.cs
+++ Services/CommandManager.cs
@@ -13,7 +13,7 @@ namespace Kinis.Services
         string Description { get; }
     }
 
-    internal class CommandManager
+    public class CommandManager
     {
         private readonly Stack<ICommand> _undoStack = new Stack<ICommand>();
         private readonly Stack<ICommand> _redoStack = new Stack<ICommand>();
@@ -22,5 +22,13 @@ namespace Kinis.Services
 
         public bool CanUndo => _undoStack.Count > 0;
         public bool CanRedo => _redoStack.Count > 0;
+
+        public void Execute(ICommand command)
+        {
+            command.Execute();
+            _undoStack.Push(command);
+            _redoStack.Clear();
+            OnStateChanged?.Invoke();
+        }
     }
 }
