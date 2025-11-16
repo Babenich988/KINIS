a1918b7a156b1f1ba8fa4a2de54b218f60306b5c
Kancerio <ghh233406@gmail.com>
2025-11-17 02:11:45 +0300

    добавлен файл класса кривых стрелок

---
+++ Models/BpmnCurvedArrow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Kinis.Model
+{
+    [Serializable]
+    public class BpmnCurvedArrow
+    { 
+    }
+}
