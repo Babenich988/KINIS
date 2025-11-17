9c797235dba5c4a1110f78c67e8c66f11ea32244
Kancerio <ghh233406@gmail.com>
2025-11-17 10:39:23 +0300

    добавлен метод нахождения расстояния до линии

--- Models/BpmnCurvedArrow.cs
+++ Models/BpmnCurvedArrow.cs
@@ -180,5 +180,39 @@ namespace Kinis.Model
             return pathPoints;
         }
 
+        // Метод нахождения расстояния до линии
+        private float DistanceToLine(PointF point, PointF lineStart, PointF lineEnd)
+        {
+            float A = point.X - lineStart.X;
+            float B = point.Y - lineStart.Y;
+            float C = lineEnd.X - lineStart.X;
+            float D = lineEnd.Y - lineStart.Y;
+
+            float dot = A * C + B * D;
+            float lenSq = C * C + D * D;
+            float param = (lenSq != 0) ? dot / lenSq : -1;
+
+            float xx, yy;
+
+            if (param < 0)
+            {
+                xx = lineStart.X;
+                yy = lineStart.Y;
+            }
+            else if (param > 1)
+            {
+                xx = lineEnd.X;
+                yy = lineEnd.Y;
+            }
+            else
+            {
+                xx = lineStart.X + param * C;
+                yy = lineStart.Y + param * D;
+            }
+
+            float dx = point.X - xx;
+            float dy = point.Y - yy;
+            return (float)Math.Sqrt(dx * dx + dy * dy);
+        }
     }
 }
