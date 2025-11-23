a0d7fbe8df294d2b089f82bc434b5ce8512af4c1
Kancerio <ghh233406@gmail.com>
2025-11-23 13:08:54 +0300

    добавлен файл класса для дорожек пула

---
+++ Models/PoolLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Kinis.Models
+{
+    public class PoolLine
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Text { get; set; } = "Lane";
+        public RectangleF Bounds { get; set; }
+        public Color FillColor { get; set; } = Color.White;
+        public Color BorderColor { get; set; } = Color.Black;
+        public List<PoolLine> ChildLines { get; set; } = new List<PoolLine>();
+        public int NestingLevel { get; set; } = 0;
+
+        public bool CanAddChildLine()
+        {
+            return NestingLevel < 2; // Максимум 3 уровня вложенности (0,1,2)
+        }
+    }
+}
