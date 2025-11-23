0a2458343b8a0dad5a344d904472abe5d5245613
Kancerio <ghh233406@gmail.com>
2025-11-23 13:11:56 +0300

    добавлен файл класс для пула

---
+++ Models/Pool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Kinis.Models
+{
+    public class Pool
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public PoolName Name { get; set; } = new PoolName();
+        public List<PoolLine> Lines { get; set; } = new List<PoolLine>();
+        public RectangleF Bounds { get; set; }
+        public Color FillColor { get; set; } = Color.White;
+        public Color BorderColor { get; set; } = Color.Black;
+
+        public Pool()
+        {
+            // Добавляем первую линию по умолчанию
+            Lines.Add(new PoolLine());
+        }
+    }
+}
