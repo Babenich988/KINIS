1a5d72784f6c567982830431c276e8d724f79449
Kancerio <ghh233406@gmail.com>
2025-12-08 19:29:42 +0300

    Revert "добавлено состояние для перетаскивания контрольных точек"
    This reverts commit 7ba5dba51ee569422f3fa8eb618bc250fc971176.

--- InfiniteCanvas.cs
+++ InfiniteCanvas.cs
@@ -53,10 +53,6 @@ namespace Kinis
 
         private BpmnBlock selectedBlock = null;
         private BpmnArrow selectedArrow = null;
-        // Состояние полей для перетаскивания
-        private bool isDraggingControlPoint = false;
-        private bool isFirstControlPoint = false;
-        private BpmnCurvedArrow curvedArrowWithControlPoint = null;
         private bool IsLaneWithinPoolBounds(PoolLine lane, BpmnBlock poolBlock)
         {
             return lane.Bounds.Y >= poolBlock.Bounds.Y + 40f && // Ниже названия
@@ -965,7 +961,7 @@ namespace Kinis
             PointF virtualPos = ScreenToVirtual(e.Location);
             this.Focus();
 
-            if (isCreatingArrow || isDraggingArrowEnd || isDraggingControlPoint)
+            if (isCreatingArrow || isDraggingArrowEnd)
                 return;
 
             if (e.Button == MouseButtons.Left)
@@ -996,19 +992,17 @@ namespace Kinis
                 var clickedCurvedArrow = GetCurvedArrowAtPoint(virtualPos);
 
                 // 1. Проверяем клик на маркеры концов обычной стрелки
-                if (clickedCurvedArrow != null && clickedCurvedArrow.IsFloating)
+                if (clickedArrow != null)
                 {
-                    bool isControlPoint;
-                    if (clickedCurvedArrow.HitTestControlPoint(virtualPos, out isControlPoint))
+                    if (clickedArrow.HitTestEndpoint(virtualPos, true) || clickedArrow.HitTestEndpoint(virtualPos, false))
                     {
-                        isDraggingControlPoint = true;
-                        curvedArrowWithControlPoint = clickedCurvedArrow;
-                        isFirstControlPoint = isControlPoint;
+                        isDraggingArrowEnd = true;
+                        isDraggingStartPoint = clickedArrow.HitTestEndpoint(virtualPos, true);
                         arrowDragStart = virtualPos;
 
                         ClearSelection();
-                        selectedElements.Add(clickedCurvedArrow);
-                        primarySelectedElement = clickedCurvedArrow;
+                        selectedElements.Add(clickedArrow);
+                        primarySelectedElement = clickedArrow;
 
                         this.Cursor = Cursors.Cross;
                         Invalidate();
@@ -1036,7 +1030,6 @@ namespace Kinis
                         return;
                     }
                 }
-
 
                 // 2. Проверяем клик на ручки изменения размера блока
                 if (clickedBlock != null)
