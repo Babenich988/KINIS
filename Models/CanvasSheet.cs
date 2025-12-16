d66f09ceb1458a09f5559d4ce3231f2faf9e25bb
Kancerio <ghh233406@gmail.com>
2025-12-16 10:49:15 +0300

    CanvasSheet.cs refactoring:

--- Models/CanvasSheet.cs
+++ Models/CanvasSheet.cs
@@ -4,15 +4,45 @@ using Kinis.Models;
 
 namespace Kinis
 {
+    /// <summary>
+    /// Модель листа холста для поддержки нескольких рабочих областей
+    /// </summary>
     public class CanvasSheet
     {
+        /// <summary>
+        /// Название листа
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Список блоков на листе
+        /// </summary>
         public List<BpmnBlock> Blocks { get; set; } = new List<BpmnBlock>();
+
+        /// <summary>
+        /// Список стрелок на листе
+        /// </summary>
         public List<BpmnArrow> Arrows { get; set; } = new List<BpmnArrow>();
+
+        /// <summary>
+        /// Масштаб отображения листа
+        /// </summary>
         public float Zoom { get; set; } = 1.0f;
+
+        /// <summary>
+        /// Смещение холста листа
+        /// </summary>
         public PointF CanvasOffset { get; set; } = new PointF(0, 0);
 
+        /// <summary>
+        /// Инициализирует новый пустой лист
+        /// </summary>
         public CanvasSheet() { }
+
+        /// <summary>
+        /// Инициализирует новый лист с указанным именем
+        /// </summary>
+        /// <param name="name">Название листа</param>
         public CanvasSheet(string name) { Name = name; }
     }
 }
