ff07b4d8013df98ba174a4bd2c3901a9dd88c9b3
Kancerio <ghh233406@gmail.com>
2025-12-16 10:29:52 +0300

    AutoSaveService.cs refactoring:

--- Services/AutoSaveService.cs
+++ Services/AutoSaveService.cs
@@ -6,6 +6,9 @@ using System.Windows.Forms;
 
 namespace Kinis.Services
 {
+    /// <summary>
+    /// Сервис автоматического сохранения проекта
+    /// </summary>
     public class AutoSaveService
     {
         private System.Timers.Timer _autoSaveTimer;
@@ -14,11 +17,28 @@ namespace Kinis.Services
         private Func<List<BpmnCurvedArrow>> _getCurvedArrows;
         private Func<string> _getAutoSaveFilePath;
 
+        /// <summary>
+        /// Получает значение, указывающее включено ли автосохранение
+        /// </summary>
         public bool IsEnabled { get; private set; }
+
+        /// <summary>
+        /// Получает интервал автосохранения в минутах
+        /// </summary>
         public int IntervalMinutes { get; private set; }
 
+        /// <summary>
+        /// Событие, возникающее при выполнении автосохранения
+        /// </summary>
         public event Action<string> AutoSavePerformed;
 
+        /// <summary>
+        /// Инициализирует сервис автосохранения
+        /// </summary>
+        /// <param name="getBlocks">Функция получения списка блоков</param>
+        /// <param name="getArrows">Функция получения списка стрелок</param>
+        /// <param name="getCurvedArrows">Функция получения списка кривых стрелок</param>
+        /// <param name="getAutoSaveFilePath">Функция получения пути файла автосохранения</param>
         public AutoSaveService(Func<List<BpmnBlock>> getBlocks, Func<List<BpmnArrow>> getArrows,
                               Func<List<BpmnCurvedArrow>> getCurvedArrows, Func<string> getAutoSaveFilePath)
         {
@@ -30,6 +50,9 @@ namespace Kinis.Services
             InitializeTimer();
         }
 
+        /// <summary>
+        /// Инициализирует таймер автосохранения
+        /// </summary>
         private void InitializeTimer()
         {
             _autoSaveTimer = new System.Timers.Timer();
@@ -37,6 +60,12 @@ namespace Kinis.Services
             _autoSaveTimer.AutoReset = true;
         }
 
+        /// <summary>
+        /// Запускает автосохранение с указанным интервалом
+        /// </summary>
+        /// <param name="intervalMinutes">Интервал в минутах (1-10)</param>
+        /// <exception cref="ArgumentException">Выбрасывается при недопустимом интервале</exception>
+        /// <exception cref="InvalidOperationException">Выбрасывается если файл не выбран</exception>
         public void Start(int intervalMinutes)
         {
             if (intervalMinutes < 1 || intervalMinutes > 10)
@@ -58,6 +87,9 @@ namespace Kinis.Services
             Console.WriteLine($"Автосохранение запущено. Интервал: {intervalMinutes} мин., Файл: {filePath}");
         }
 
+        /// <summary>
+        /// Останавливает автосохранение
+        /// </summary>
         public void Stop()
         {
             IsEnabled = false;
@@ -65,6 +97,10 @@ namespace Kinis.Services
             Console.WriteLine("Автосохранение остановлено");
         }
 
+        /// <summary>
+        /// Обновляет интервал автосохранения
+        /// </summary>
+        /// <param name="intervalMinutes">Новый интервал в минутах</param>
         public void UpdateInterval(int intervalMinutes)
         {
             if (IsEnabled)
@@ -78,11 +114,17 @@ namespace Kinis.Services
             }
         }
 
+        /// <summary>
+        /// Обработчик события таймера автосохранения
+        /// </summary>
         private void OnAutoSaveTimerElapsed(object sender, ElapsedEventArgs e)
         {
             PerformAutoSave();
         }
 
+        /// <summary>
+        /// Выполняет автоматическое сохранение проекта
+        /// </summary>
         private void PerformAutoSave()
         {
             try
@@ -125,6 +167,10 @@ namespace Kinis.Services
             }
         }
 
+        /// <summary>
+        /// Получает информацию о состоянии автосохранения
+        /// </summary>
+        /// <returns>Строка с информацией об автосохранении</returns>
         public string GetAutoSaveInfo()
         {
             if (!IsEnabled)
@@ -148,6 +194,9 @@ namespace Kinis.Services
             }
         }
 
+        /// <summary>
+        /// Освобождает ресурсы сервиса
+        /// </summary>
         public void Dispose()
         {
             _autoSaveTimer?.Stop();
