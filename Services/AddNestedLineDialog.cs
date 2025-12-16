7e3a42ba9892c1828e320b6a559ca923144db103
Kancerio <ghh233406@gmail.com>
2025-12-16 10:50:25 +0300

    AddNestedLineDialog.cs refactoring:

--- Services/AddNestedLineDialog.cs
+++ Services/AddNestedLineDialog.cs
@@ -3,37 +3,55 @@ using System.Windows.Forms;
 
 namespace Kinis
 {
+    /// <summary>
+    /// Диалоговое окно для добавления вложенной дорожки в пул
+    /// </summary>
     public partial class AddNestedLineDialog : Form
     {
+        /// <summary>
+        /// Получает название вложенной дорожки
+        /// </summary>
         public string LineName { get; private set; }
+
+        /// <summary>
+        /// Получает название родительской дорожки
+        /// </summary>
         public string ParentLaneName { get; private set; }
 
+        /// <summary>
+        /// Инициализирует диалог для добавления вложенной дорожки
+        /// </summary>
+        /// <param name="parentLaneName">Название родительской дорожки</param>
         public AddNestedLineDialog(string parentLaneName)
         {
             ParentLaneName = parentLaneName;
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Инициализирует компоненты пользовательского интерфейса диалогового окна
+        /// </summary>
         private void InitializeComponent()
         {
             this.Text = "Добавить вложенную дорожку";
             this.Size = new System.Drawing.Size(350, 150);
             this.StartPosition = FormStartPosition.CenterParent;
 
-            // Label
+            // Label с информацией о родительской дорожке
             var label = new Label();
             label.Text = $"Вложенная дорожка для: {ParentLaneName}";
             label.Location = new System.Drawing.Point(20, 15);
             label.Size = new System.Drawing.Size(300, 20);
             this.Controls.Add(label);
 
+            // Label с инструкцией
             var label2 = new Label();
             label2.Text = "Введите название вложенной дорожки:";
             label2.Location = new System.Drawing.Point(20, 40);
             label2.Size = new System.Drawing.Size(300, 20);
             this.Controls.Add(label2);
 
-            // TextBox
+            // TextBox для ввода названия дорожки
             var textBox = new TextBox();
             textBox.Location = new System.Drawing.Point(20, 65);
             textBox.Size = new System.Drawing.Size(290, 20);
@@ -41,7 +59,7 @@ namespace Kinis
             textBox.Text = "Вложенная дорожка";
             this.Controls.Add(textBox);
 
-            // Кнопки
+            // Кнопка подтверждения
             var btnOk = new Button();
             btnOk.Text = "OK";
             btnOk.Location = new System.Drawing.Point(130, 100);
@@ -53,6 +71,7 @@ namespace Kinis
             };
             this.Controls.Add(btnOk);
 
+            // Кнопка отмены
             var btnCancel = new Button();
             btnCancel.Text = "Отмена";
             btnCancel.Location = new System.Drawing.Point(210, 100);
